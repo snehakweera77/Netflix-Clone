1ce07b6b109aa19ac828e13c7fb173f610c5f9f7
snehakweera77 <snehakweera77@gmail.com>
2021-11-05 20:18:27 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
-
+import Login from "./pages/login/Login";
 function App() {
   return (
     <div className="app">
-      <Register />
+      <Login />
 
       {/*<Row
         title="NETFLIX ORIGINAL"
