497fe41c2d2e5bccdf82a434b3708581590b6b3f
snehakweera77 <snehakweera77@gmail.com>
2021-11-05 22:36:02 +0530

    index.js created, db connected

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
+const dotenv = require("dotenv");
 
+const app = express();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
 app.listen(8800, () => {
   console.log("server is running");
 });
