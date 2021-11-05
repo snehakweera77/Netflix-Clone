174bcfacc6d83f90057dc85c39d4cd0aff42b80f
snehakweera77 <snehakweera77@gmail.com>
2021-11-05 18:58:41 +0530

    Movie List and list item

---
+++ src/components/list/List.js
@@ -0,0 +1,58 @@
+import React, { useRef, useState } from "react";
+import "./List.css";
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from "@material-ui/icons";
+import ListItem from "../listItem/ListItem";
+
+function List() {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [clickLimit, setClickLimit] = useState(window.innerWidth / 230);
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 10 - clickLimit) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
+  return (
+    <div className="list">
+      <span className="list__title">Continue to watch</span>
+      <div className="list__wrapper">
+        <ArrowBackIosOutlined
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
+          <ListItem index={9} />
+          <ListItem index={10} />
+        </div>
+        <ArrowForwardIosOutlined
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default List;
