07be6f0026803445703908583ba89d3393166a1f
snehakweera77 <snehakweera77@gmail.com>
2021-11-06 20:11:37 +0530

    Delete a movie

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -36,4 +36,18 @@ router.put("/:id", verify, async (req, res) => {
     res.status(403).json("You are not allowed!");
   }
 });
+
+//DELETE
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await Movie.findByIdAndDelete(req.params.id);
+      res.status(200).json("The movie has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
 module.exports = router;
