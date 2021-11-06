dc0a4f1277d6659f7ecfb4cf80a9e4b2d2e8bd4a
snehakweera77 <snehakweera77@gmail.com>
2021-11-06 19:33:17 +0530

    post new movie

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
 
 const app = express();
 dotenv.config();
@@ -19,6 +20,8 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+
 app.listen(8800, () => {
   console.log("server is running");
 });
---
+++ backend/routes/movies.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const Movie = require("../models/Movie");
+const verify = require("../verifyToken");
+
+//CREATE
+
+router.post("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const newMovie = new Movie(req.body);
+    try {
+      const savedMovie = await newMovie.save();
+      res.status(201).json(savedMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+module.exports = router;
