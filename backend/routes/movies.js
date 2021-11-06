77658e9140cba77a151d764f63e7759de99d30d7
snehakweera77 <snehakweera77@gmail.com>
2021-11-06 19:57:06 +0530

    Update the movie details

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -3,7 +3,6 @@ const Movie = require("../models/Movie");
 const verify = require("../verifyToken");
 
 //CREATE
-
 router.post("/", verify, async (req, res) => {
   if (req.user.isAdmin) {
     const newMovie = new Movie(req.body);
@@ -18,4 +17,23 @@ router.post("/", verify, async (req, res) => {
   }
 });
 
+//UPDATE
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updatedMovie = await Movie.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
 module.exports = router;
