b3502594c3a814fc80d6ea9f8fcb4d760aa7592f
snehakweera77 <snehakweera77@gmail.com>
2021-11-06 20:42:38 +0530

    get all movies

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -83,4 +83,17 @@ router.get("/random", verify, async (req, res) => {
   }
 });
 
+//GET ALL
+router.get("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const movies = await Movie.find();
+      res.status(200).json(movies.reverse());
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
 module.exports = router;
