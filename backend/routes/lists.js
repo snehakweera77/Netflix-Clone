c9deb2645d76261d1255897c1cdf674b0d480a4e
snehakweera77 <snehakweera77@gmail.com>
2021-11-06 21:23:51 +0530

    delete a movie list

--- backend/routes/lists.js
+++ backend/routes/lists.js
@@ -17,4 +17,17 @@ router.post("/", verify, async (req, res) => {
   }
 });
 
+//DELETE
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await List.findByIdAndDelete(req.params.id);
+      res.status(201).json("The list has been delete...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
 module.exports = router;
