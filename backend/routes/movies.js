3aee0e0da0af109493af56d7009e4383a2968055
snehakweera77 <snehakweera77@gmail.com>
2021-11-06 20:38:06 +0530

    get random movie

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -61,4 +61,26 @@ router.get("/find/:id", verify, async (req, res) => {
   }
 });
 
+//GET RANDOM
+router.get("/random", verify, async (req, res) => {
+  const type = req.query.type;
+  let movie;
+  try {
+    if (type === "series") {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: true } },
+        { $sample: { size: 1 } },
+      ]);
+    } else {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: false } },
+        { $sample: { size: 1 } },
+      ]);
+    }
+    res.status(200).json(movie);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
