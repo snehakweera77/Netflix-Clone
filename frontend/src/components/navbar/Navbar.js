b2a20a0dff1ee98181d18408f409ced481ccb866
snehakweera77 <snehakweera77@gmail.com>
2021-11-10 12:09:41 +0530

    navbar logout

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,55 +1,52 @@
-import React, { useState, useEffect } from "react";
-import "./Navbar.css";
+import React, { useState, useContext } from "react";
+import "./Navbar.scss";
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../authContext/AuthContext";
+import { logout } from "../../authContext/AuthActions";
 
 function Navbar() {
-  const [show, handleShow] = useState(false);
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 100) {
-        handleShow(true);
-      } else handleShow(false);
-    });
-    return () => {
-      window.removeEventListener("scroll");
-    };
-  }, []);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const { dispatch } = useContext(AuthContext);
 
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
   return (
-    <div className={`navbar ${show && "navbar__black"}`}>
-      <div className="navbar__left">
-        <img
-          className="navbar__logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
-          alt="Netflix Logo"
-        />
-        <Link to="/" className="link">
-          <span>Homepage</span>
-        </Link>
-        <Link to="/series" className="link">
-          <span className="navbarmainLinks">Series</span>
-        </Link>
-        <Link to="/movies" className="link">
-          <span className="navbarmainLinks">Movies</span>
-        </Link>
-        <span>New and Popular</span>
-        <span>My List</span>
-      </div>
-      <div className="navbar__right">
-        <Search className="navbar__right__icon" />
-        <span>KID</span>
-        <Notifications className="navbar__right__icon" />
-        <img
-          className="navbar__avatar"
-          src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
-          alt="Netflix avatar"
-        />
-        <div className="navbar__right__profile">
-          <ArrowDropDown className="navbar__right__icon" />
-          <div className="navbar__right__profile__options">
-            <span>Settings</span>
-            <span>Logout</span>
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+      <div className="container">
+        <div className="left">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+          <Link to="/" className="link">
+            <span>Homepage</span>
+          </Link>
+          <Link to="/series" className="link">
+            <span className="navbarmainLinks">Series</span>
+          </Link>
+          <Link to="/movies" className="link">
+            <span className="navbarmainLinks">Movies</span>
+          </Link>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
+        <div className="right">
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img
+            src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+            alt=""
+          />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span onClick={() => dispatch(logout())}>Logout</span>
+            </div>
           </div>
         </div>
       </div>
