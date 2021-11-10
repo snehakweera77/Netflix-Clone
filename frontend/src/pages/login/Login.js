73c626bef9ee122226bfe33c73f583b2328651cf
snehakweera77 <snehakweera77@gmail.com>
2021-11-10 11:58:44 +0530

    login done

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,19 +1,29 @@
-import React, { useRef, useState } from "react";
-import "./Login.css";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../authContext/AuthContext";
+import { login } from "../../authContext/apiCalls";
+import "./Login.scss";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { dispatch } = useContext(AuthContext);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login({ email, password }, dispatch);
+  };
   return (
     <div className="login">
-      <div className="login__header">
-        <img
-          className="login__header__logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-          alt=""
-        />
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+        </div>
       </div>
-      <div className="login__container">
+      <div className="container">
         <form>
           <h1>Sign In</h1>
           <input
@@ -26,7 +36,9 @@ function Login() {
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
           />
-          <button className="login__container__loginButton">Sign In</button>
+          <button className="loginButton" onClick={handleLogin}>
+            Sign In
+          </button>
           <span>
             New to Netflix? <b>Sign up now.</b>
           </span>
